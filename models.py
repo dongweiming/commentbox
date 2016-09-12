3c3bab64e08fdfa6fb076876fefaaeabe949b335
dongweiming <ciici1234@hotmail.com>
2016-09-12 10:59:11 +0800

    add url property

--- models.py
+++ models.py
@@ -4,6 +4,10 @@ from mongoengine import (Document, StringField, DateTimeField, ReferenceField,
                          IntField, ListField, DoesNotExist, connect)
 from config import DB_HOST, DB_PORT, DATABASE_NAME
 
+ARTIST_URL = 'http://music.163.com/#/artist?id={}'
+SONG_URL = 'http://music.163.com/#/song?id={}'
+USER_URL = 'http://music.163.com/#/user/home?id={}'
+
 
 def lazy_connect():
     connect(DATABASE_NAME, host=DB_HOST, port=DB_PORT)
@@ -34,12 +38,24 @@ class Artist(BaseModel):
     picture = StringField()
     songs = ListField(ReferenceField('Song'))
 
+    @property
+    def url(self):
+        return ARTIST_URL.format(self.id)
+
 
 class Song(BaseModel):
     name = StringField()
     comment_count = IntField()
     comments = ListField(ReferenceField('Comment'))
     artist = ReferenceField('Artist')
+
+    @property
+    def url(self):
+        return SONG_URL.format(self.id)
+
+    @property
+    def artist_url(self):
+        return self.artist.url
 
 
 class Comment(BaseModel):
@@ -48,10 +64,26 @@ class Comment(BaseModel):
     user = ReferenceField('User')
     song = ReferenceField('Song')
 
+    @property
+    def user_url(self):
+        return self.user.url
+
+    @property
+    def song_url(self):
+        return self.song.url
+
+    @property
+    def user_url(self):
+        return self.song.artist_url
+
 
 class User(BaseModel):
     name = StringField()
     picture = StringField()
+
+    @property
+    def url(self):
+        return USER_URL.format(self.id)
 
 
 class Process(BaseModel):
